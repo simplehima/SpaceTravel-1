2bb2a0ae0711770d2aa985457392682cb97b7318
IbrahimAzab <54166348+simplehima@users.noreply.github.com>
2024-04-21 08:44:12 +0200

    added pause

--- Assets/Scripts/UI Managment.cs
+++ Assets/Scripts/UI Managment.cs
@@ -6,34 +6,49 @@ using UnityEngine.SceneManagement;
 
 public class UIManagment : MonoBehaviour
 {
+    private bool isPaused = false; // Flag to track if the game is paused
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
+    }
 
     public void OnClickStart()
     {
         SceneManager.LoadScene("Level1");
-
-
     }
 
     public void OnClick2Start()
     {
         SceneManager.LoadScene("Level2");
-
-
     }
+
     public void OnClickQuit()
     {
         Application.Quit();
+    }
+
+    public void OnClickPause()
+    {
+        // Toggle the pause state
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            // Pause the game
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            // Resume the game
+            Time.timeScale = 1f;
+        }
     }
 }
